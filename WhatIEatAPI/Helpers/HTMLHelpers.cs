b3fd7446c54fac070ac12b72f481242b4ff27880
Jurij <jzilcov@gmail.com>
2017-08-09 13:39:32 +0100

    Release notification

---
+++ WhatIEatAPI/Helpers/HTMLHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace WhatIEatAPI.Helpers
+{
+    public static class HTMLHelpers
+    {
+        public static IHtmlContent ProcessName(this IHtmlHelper htmlHelper)
+            => new HtmlString(Process.GetCurrentProcess().ProcessName);
+
+        public static IHtmlContent HelloWorldHTMLString(this IHtmlHelper htmlHelper)
+            => new HtmlString("<strong>Hello World</strong>");
+
+        public static IHtmlContent IsDebug(this IHtmlHelper htmlHelper)
+        {
+            #if DEBUG
+                return new HtmlString("DEBUG - IISExpress");
+            #else
+                return new HtmlString("RELEASE - IIS");
+            #endif
+        }
+    }
+}
